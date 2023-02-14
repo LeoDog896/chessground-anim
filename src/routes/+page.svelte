860449ec28b939fa3ef75a0d3dceae6c3b498a4f
Tristan F <LeoDog896@hotmail.com>
2023-02-14 15:22:18 +0000

    feat: board

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,38 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import { Chessground } from "chessground/chessground"
+    import { onMount } from "svelte"
+    import "chessground/assets/chessground.base.css"
+    import "chessground/assets/chessground.brown.css"
+    import "chessground/assets/chessground.cburnett.css"
+
+    let div: HTMLDivElement;
+
+    onMount(() => {
+        const ground = Chessground(div, {
+            movable: {
+                free: true
+            },
+            animation: {
+                enabled: true,
+                duration: 200
+            }
+        })
+    })
+</script>
+
+<div class="container">
+    <div class="child" bind:this={div}></div>
+</div>
+
+<style>
+    div.container {
+        width: 500px;
+        height: 500px;
+    }
+
+    div.child {
+        display: table;
+        width: 100%;
+        height: 100%;
+    }
+</style>
