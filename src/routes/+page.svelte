9eefee183c409f29959037d3b71bbbabdc8a3dcf
Tristan F <LeoDog896@hotmail.com>
2023-02-15 20:29:28 -0500

    feat: centering

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -20,11 +20,25 @@
     })
 </script>
 
-<div class="container">
-    <div class="child" bind:this={div}></div>
-</div>
+<main>
+    <div class="container">
+        <div class="child" bind:this={div}></div>
+    </div>
+</main>
 
 <style>
+    :global(body) {
+        margin: 0;
+    }
+
+    main {
+        /* center */
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        
+    }
     div.container {
         width: 500px;
         height: 500px;
