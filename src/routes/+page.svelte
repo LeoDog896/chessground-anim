e4fc068a23f4fd88313f3e1a9cc78f657e537a89
Tristan F <LeoDog896@hotmail.com>
2023-06-18 11:46:27 -0400

    feat: valid move generation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,17 +6,35 @@
     import "chessground/assets/chessground.brown.css"
     import "chessground/assets/chessground.cburnett.css"
     import type { Key } from "chessground/types";
-    import { Chess } from 'chess.js'
+    import { Chess, SQUARES } from 'chess.js'
 
     let div: HTMLDivElement;
 
     let history: [fen: string, move: Key[]][] = []
     let ground: Api;
 
+
+    // adapted from lichess-examples
+    function toDests(fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"): Map<Key, Key[]> {
+        const whiteChess = new Chess(fen + " w KQkq - 0 2");
+        const blackChess = new Chess(fen + " b KQkq - 0 2");
+        
+        const dests = new Map();
+        SQUARES.forEach(s => {
+            const whiteMoves = whiteChess.moves({square: s, verbose: true});
+            const blackMoves = blackChess.moves({square: s, verbose: true});
+
+            if (whiteMoves.length) dests.set(s, whiteMoves.map(m => m.to));
+            if (blackMoves.length) dests.set(s, blackMoves.map(m => m.to));
+        });
+        return dests;
+    }
+
     onMount(() => {
         ground = Chessground(div, {
             movable: {
-                free: true
+                free: true,
+                dests: toDests()
             },
             animation: {
                 enabled: true,
@@ -38,7 +56,12 @@
         if (history.length === 0) {
             ground.set({
                 fen: "start",
-                lastMove: undefined
+                lastMove: undefined,
+                check: undefined,
+                movable: {
+                    free: true,
+                    dests: toDests()
+                }
             })
         } else {
             const [fen, lastMove] = history[history.length - 1];
@@ -59,7 +82,11 @@
             ground.set({
                 fen,
                 lastMove,
-                check
+                check,
+                movable: {
+                    free: true,
+                    dests: toDests(fen)
+                }
             })
         }
     }
